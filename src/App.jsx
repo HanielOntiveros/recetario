2f1c4fc56a768ca0c1e38f26fc11a11f75d486b1
Haniel Ontiveros <haniel97@hotmail.com>
2023-01-12 13:19:10 -0700

    Adding p tag

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
         <h1 className="text-4xl font-bold text-center  ">
           Recetario Haniel + Gaizka
         </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga voluptates aliquam sapiente ipsam aperiam quasi ab natus libero incidunt quas temporibus distinctio amet quae vero, dolorem quidem placeat mollitia totam.</p>
       </div>
     </>
   );
