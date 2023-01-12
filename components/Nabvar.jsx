e3f0ab075a2cc6c3b6ec5206be1d6a1e0419bba7
Haniel Ontiveros <haniel97@hotmail.com>
2023-01-12 11:09:00 -0700

    change color

--- components/Nabvar.jsx
+++ components/Nabvar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Nabvar() {
   return (
-    <div className="bg-red-500 flex justify-between items-center w-full px-6 py-6">
+    <div className="bg-green-500 flex justify-between items-center w-full px-6 py-6">
       <div>Logo</div>
       <div>Cards</div>
     </div>
